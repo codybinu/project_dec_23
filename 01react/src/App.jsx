ca40bb992e1accbde29b4291b1878871404ba693
codybinu <binayak.bidyasagar99@gmail.com>
2023-12-12 21:17:07 +0530

    commiting code

--- 01react/src/App.jsx
+++ 01react/src/App.jsx
@@ -2,8 +2,14 @@
 import Binu from "./Binu";
 
 function App() {
+  const userName = "chai peeke code likho";
   return (
-    <Binu />
+    <>
+      <Binu />
+      <h2>code kaise karega tu? : {userName}</h2>  
+      {/* whatever is there inside curly bracket is called as evaluated expression. as the {} does not take any direct js code like if statement etc*/}
+    </>
+
   )
 }
 
