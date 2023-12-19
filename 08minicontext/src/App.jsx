604c1990daf78792239e9dd3d95f72f122599b7e
codybinu <binayak.bidyasagar99@gmail.com>
2023-12-19 23:02:23 +0530

    08minicontext half completed

---
+++ 08minicontext/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+
+function App() {
+  
+
+  return (
+    <UserContextProvider>
+     <h1>binu working with context</h1>
+    </UserContextProvider>
+  )
+}
+
+export default App
